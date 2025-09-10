39564d21bc39b88b8d7838d4546cd444978f7cce
Renee Tsui <reneetsui@Renees-MacBook-Air.local>
2025-09-10 16:55:04 -0400

    added comments

--- fa25-cs391-s1-hw0-assessment 2/CSVCalculatorHandler.py
+++ fa25-cs391-s1-hw0-assessment 2/CSVCalculatorHandler.py
@@ -3,20 +3,24 @@ from Calculator import Calculator
 
 class CSVCalculatorHandler:
     def __init__(self):
+        # create calculator object
         self.calc = Calculator()
+        #store history
         self.history = []
 
     def process_csv(self, filenames, output_prefix="output"):
         for i, filename in enumerate(filenames):
             with open(filename, newline='') as f_in, open(f"{output_prefix}_{i}.csv", "w", newline='') as f_out:
+                #read the file 
                 read = csv.reader(f_in)
+                #write header row
                 writer = csv.writer(f_out)
                 writer.writerow(["result", "error"])
-
+                # perform operation and process each line
                 for row in read:
                     *nums, op = row
                     nums = [float(x) for x in nums]
-
+                    #match opperators to the calculation
                     result, error = None, 0
                     if op == "add" and len(nums) >= 2:
                         result = self.calc.add(*nums)
@@ -35,14 +39,16 @@ class CSVCalculatorHandler:
                         error = 2
                     else:
                         error = 3
-
+                    #write result and error code to output file
                     writer.writerow([result if result is not None else "", error])
+                    #save to history
                     self.save_to_history(filename, ",".join(row), result, error)
-
+# add one processed row to the history list
     def save_to_history(self, filename, operation, result, error_code):
         self.history.append([filename, operation, result if result is not None else "", error_code])
 
     def history_export(self, export_filename):
+        #write full history to csv file
         with open(export_filename, "w", newline='') as f:
             writer = csv.writer(f)
             writer.writerow(["input_file", "operation", "result", "error"])
--- fa25-cs391-s1-hw0-assessment 2/Calculator.py
+++ fa25-cs391-s1-hw0-assessment 2/Calculator.py
@@ -1,18 +1,23 @@
 class Calculator:
+    # adds all numbers together using built in feature
     def add(self, *nums):
         return sum(nums)
+    # subtracts in order for first to last
     def subtract(self,*nums):
         result = nums[0]
         for n in nums[1:]:
             result-=n
         return result
+    #multiplies all numbers
     def multiply(self, *nums):
         result =1
         for n in nums:
             result*=n
         return result
+    # divides first by second number
     def divide(self, a, b):
         return a/b
+    # divides first by second
     def exponentiate(self, a, b):
         return a ** b
     
